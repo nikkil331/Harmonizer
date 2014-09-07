c83544e9f68aae36de707aabb0449c770ac2ff36
rigeljs <rigeljs@gmail.com>
2014-09-07 17:15:38 -0400

    change simpleHarmony to transpose by diatonic 3rd, unison if non-diatonic

--- simpleHarmony.py
+++ simpleHarmony.py
@@ -3,9 +3,15 @@ from music21 import *
 handelmovement = corpus.parse("handel/hwv56")
 vio = handelmovement[1]
 harmony = stream.Stream()
-
-for note in vio:
-	harmony.append(note.transpose('M3'))
+keySig = vio.getKeySignatures()[0]
+scaleFromKey = scale.MajorScale(keySig.pitchAndMode[0]) 
+for note in vio.flat.notes:
+	degree = scaleFromKey.getScaleDegreeFromPitch(note)
+	if degree is None: 
+		harmony.append(note)
+	else:
+		inter = interval.notesToInterval(note, scaleFromKey.pitchFromDegree(degree + 2))
+		harmony.append(note.transpose(inter))
 
 score = stream.Score([harmony,vio])
 
