2027d9775e1aab6eb1408fa6cc4d324ad075d67c
rigeljs <rigeljs@gmail.com>
2014-09-07 16:06:47 -0400

    actually transposes now

--- simpleHarmony.py
+++ simpleHarmony.py
@@ -3,4 +3,8 @@ from music21 import *
 handelmovement = corpus.parse("handel/hwv56")
 vio = handelmovement[1]
 harmony = stream.Stream()
-third = interval.GenericInterval(3)
+
+for note in vio:
+	harmony.append(note.transpose('M3'))
+
+harmony.show()
