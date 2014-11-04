55f967755629a7630e025af1954d6d6e6270214f
nikkil331 <limni@seas.upenn.edu>
2014-11-04 15:26:36 -0500

    prob for translating melody to itself is 1

--- translation_model.py
+++ translation_model.py
@@ -18,14 +18,16 @@ class TranslationModel(object):
 			self._tm[melody][harmony] = prob
 
 	def get_probability(self, melody, harmony):
-		if melody not in self._tm or harmony not in self._tm[melody]:
+		if (melody not in self._tm and harmony is not melody) or if harmony not in self._tm[melody]:
 			raise Exception('No melody/harmony pair for {0}, {1}'.format(melody, harmony))
+		elif melody not in self._tm and harmony is melody:
+			return 1.0
 		else:
 			return self._tm[melody][harmony]
 
 	def get_harmonies(self, melody):
 		if melody not in self._tm:
-			return []
+			return [(melody, 1.0)]
 		else:
 			return [(k,v) for k,v in self._tm[melody].iteritems()]
 
