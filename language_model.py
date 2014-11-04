87b08451234bc537d6636193f619b0870ccf6e4d
rigeljs <rigeljs@gmail.com>
2014-11-04 12:30:53 -0500

    add language model class

---
+++ language_model.py
@@ -0,0 +1,39 @@
+class LanguageModel(object):
+
+	def __init__(self, path):
+		self._lm = {}
+		self._read_file(path)
+
+	def _read_file(self, path):
+		f = open(path, 'r')
+		for line in f:
+			context_string, note, prob = line.split(" ||| ")
+			context = tuple(context_string.split())
+			self.add_to_model(context, note, prob)
+
+	def add_to_model(self, context, note, prob):
+		if context not in self._lm:
+			self._lm[context] = {note : prob}
+		else:
+			self._lm[context][note] = prob
+
+	def get_probability(self, context, note):
+		if context not in self._lm:
+			return 1e-5
+		elif note not in self._lm[context]:
+			return self._lm[context]["<UNK>"]
+		else:
+			return self._lm[context][note]
+
+	def get_contexts(self):
+		return self._lm.keys()
+
+	def get_notes_for_context(self, context):
+		return self._lm[context].keys()
+
+def main():
+	lm = LanguageModel("data/bass_language_model_major.txt")
+	print lm.get_notes_for_context(('G3', 'C4', 'R', 'C3', 'D3'))
+
+if __name__ == "__main__":
+    main()
