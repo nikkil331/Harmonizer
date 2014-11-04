f0a0e8e21ceeb1937d9b575a9a7da7e07fa283f3
rigeljs <rigeljs@gmail.com>
2014-11-04 14:05:06 -0500

    refactor translation model generator to use translation model class

--- translation_model.py
+++ translation_model.py
@@ -21,7 +21,7 @@ class TranslationModel(object):
 		if melody not in self._tm or harmony not in self._tm[melody]:
 			raise Exception('No melody/harmony pair for {0}, {1}'.format(melody, harmony))
 		else:
-			return self._lm[melody][harmony]
+			return self._tm[melody][harmony]
 
 	def get_harmonies(self, melody):
 		if melody not in self._tm:
@@ -29,6 +29,13 @@ class TranslationModel(object):
 		else:
 			return [(k,v) for k,v in self._tm[melody].iteritems()]
 
+	def write_to_file(self,path):
+		f = open(path, 'w')
+		for melody_note in self._tm:
+			for harmony_note in self._tm[melody_note]:
+				output_line = ''.join([str(melody_note), ' ||| ', str(harmony_note), ' ||| ', \
+					str(self.get_probability(melody_note, harmony_note)), '\n'])
+				f.write(output_line)
 
 
 def main():
--- translation_model_generator.py
+++ translation_model_generator.py
@@ -2,20 +2,19 @@ from music21 import *
 import optparse
 import re
 import sys
+from translation_model import TranslationModel
 
 class TranslationModelGenerator(object):
 
-	def __init__(self, mode='major', melody_part=0, harmony_part=1, \
-				 output_file='data/translation_model.txt', training_composers=['bach', 'handel']):
+	def __init__(self, mode='major', melody_part=0, harmony_part=1, training_composers=['bach', 'handel']):
 		self._mode = 'major' if mode == 'major' else 'minor'
 		self._melody_part = melody_part
 		self._harmony_part = harmony_part
-		self._output_file = output_file
 		self._training_paths = []
 		#for composer in training_composers:
 		#	self._training_paths += corpus.getComposer(composer)
 		self._training_paths = corpus.getBachChorales()
-		self._tm = None
+		self._tm_counts = None
 
 	def transpose(self, stream):
 		curr_pitch = stream.analyze('key').pitchAndMode[0].name
@@ -42,11 +41,11 @@ class TranslationModelGenerator(object):
 			harmony_offset = harmony_note.offset
 			melody_notes = melody.getElementsByOffset(harmony_offset, offsetEnd=harmony_offset + harmony_note.duration.quarterLength, 
 												     mustFinishInSpan=False, mustBeginInSpan=False)
-			if harmony_rep not in self._tm:
+			if harmony_rep not in self._tm_counts:
 				d = {}
-				self._tm[harmony_rep] = d
+				self._tm_counts[harmony_rep] = d
 			else:
-				d = self._tm[harmony_rep]
+				d = self._tm_counts[harmony_rep]
 
 			for melody_note in melody_notes.flat.notesAndRests:
 				if not melody_note.isNote:
@@ -58,16 +57,18 @@ class TranslationModelGenerator(object):
 				else:
 					d[melody_rep] += 1
 
-	def _update_probs_from_counts(self):
-		for harmony_note in self._tm:
-			total_notes_harmonized = sum(self._tm[harmony_note].values())
-			harmony_counts = self._tm[harmony_note].items()[:]
+	def _create_tm_from_counts(self):
+		tm = TranslationModel()
+		for harmony_note in self._tm_counts:
+			total_notes_harmonized = sum(self._tm_counts[harmony_note].values())
+			harmony_counts = self._tm_counts[harmony_note].items()[:]
 			for (melody_note, count) in harmony_counts:
 				prob = count / float(total_notes_harmonized)
-				self._tm[harmony_note][melody_note] = prob
+				tm.add_to_model(melody_note,harmony_note,prob)
+		return tm
 
 	def generate_tm(self):
-		self._tm = {}
+		self._tm_counts = {}
 		num_songs = 0
 		num_songs_without_part = 0
 		for path in self._training_paths:
@@ -85,23 +86,19 @@ class TranslationModelGenerator(object):
 			except Exception, e:
 				num_songs_without_part += 1
 
-		self._update_probs_from_counts()
-
 		print "Number of songs: {0}".format(num_songs)
 		print "Number of songs without {0} : {1}".format(self._harmony_part, num_songs_without_part)
 
-	def write_tm(self):
-		f = open(self._output_file, 'w')
-		for harmony_note in self._tm:
-			for melody_note in self._tm[harmony_note]:
-				output_line = ''.join([str(melody_note), ' ||| ', str(harmony_note), ' ||| ', str(self._tm[harmony_note][melody_note]), '\n'])
-				f.write(output_line)
+		return self._create_tm_from_counts()
+
+
 
 
 def main():
-	tm_generator = TranslationModelGenerator(melody_part="Soprano", harmony_part="Bass", output_file='data/bass_translation_model_major.txt')
-	tm_generator.generate_tm()
-	tm_generator.write_tm()
+	tm = TranslationModel()
+	tm_generator = TranslationModelGenerator(melody_part="Soprano", harmony_part="Bass")
+	tm = tm_generator.generate_tm()
+	tm.write_to_file('data/bass_translation_model_major.txt')
 
 if __name__ == "__main__":
     main()
