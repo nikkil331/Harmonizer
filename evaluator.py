c1253eab6f84d779a8224d4e01b0278dd85ad66a
rigeljs <rigeljs@gmail.com>
2015-03-17 17:33:00 -0400

    implement theory based evaluator for entire score

--- evaluator.py
+++ evaluator.py
@@ -12,6 +12,31 @@ optparser.add_option("--lm", dest="lm_path", default="data/language_model_major.
 optparser.add_option("--output", dest="output_file", default="evaluation_scores.txt", help="Destination of results")
 (opts, _) = optparser.parse_args()
 
+class ScoreEvaluator(object):
+	def __init__(self, song):
+		self.song = song
+
+	def evaluate(self):
+		num_measures = len(self.song.parts[0].getElementsByClass('Measure'))
+		theoryAnalysis.theoryAnalyzer.identifyParallelFifths(self.song)
+		p_fifths = self.song.analysisData['ResultDict']['parallelFifths']
+		theoryAnalysis.theoryAnalyzer.identifyImproperDissonantIntervals(self.song)
+		improper_dissonant_intervals = self.song.analysisData['ResultDict']['improperDissonantIntervals']
+		theoryAnalysis.theoryAnalyzer.identifyImproperResolutions(self.song)
+		improper_resolutions = self.song.analysisData['ResultDict']['improperResolution']
+		#theoryAnalysis.theoryAnalyzer.identifyOpensIncorrectly(self.song)
+		#theoryAnalysis.theoryAnalyzer.identifyClosesIncorrectly(self.song)
+		print "# parallel fifths per measure: {0}".format(float(len(p_fifths))/num_measures)
+		print "# improper dissonant intervals per measure: {0}".format(float(len(improper_dissonant_intervals))/num_measures)
+		print "# improper resolutions per measure: {0}".format(float(len(improper_resolutions))/num_measures)
+		# print "opens incorrectly: {0}".format(self.song.analysisData['ResultDict']['opensIncorrectly'])
+		# print "closes incorrectly: {0}".format(self.song.analysisData['ResultDict']['closesIncorrectly'][0].text)
+		# print "Parallel fifths:"
+		# print '\n'.join(map(lambda x: x.text, p_fifths))
+		# print "Improper dissonances:"
+		# print '\n'.join(map(lambda x: x.text, improper_dissonant_intervals))
+
+
 class Evaluator(object):
 
 	def __init__(self):
@@ -83,21 +108,33 @@ class Evaluator(object):
 		lm_score = self.evaluate_language_model(lm)
 		return phrase_weight*phrase_score + note_weight*notes_score + lm_weight*lm_score
 
-def main():
-	existing_parts = ["Soprano", "Alto", "Tenor", "Bass"]
-	generated_parts = ["Alto", "Tenor", "Bass"]
-	for p1 in existing_parts:
-		for p2 in generated_parts:
-			if p1 == p2:
-				continue
-			print p1, p2
-			e = Evaluator()
-			lm_major = LanguageModel(path="data/{0}_language_model_major.txt".format(p2), part=p2)
-			tm_major = TranslationModel(phrase_path="data/{0}_{1}_translation_model_major_rhythm.txt".format(p1, p2), 
-										note_path="data/{0}_{1}_translation_model_major.txt".format(p1, p2),
-										harmony_part=p2, melody_part=p1)
 
-			#print "TM major: ", str(e.evaluate_translation_model(tm_major))
-			print "unweighted with phrases: ", str(e.evaluate_combined(tm_major, lm_major, 0.5726547297805934, 0.061101102321016725, 0.0020716756164958113))
+def main():
+	'''e = Evaluator()
+
+	tm_soprano_bass = TranslationModel(phrase_path="data/Soprano_Bass_translation_model_major_rhythm.txt", note_path="data/Soprano_Bass_translation_model_major.txt", harmony_part="Bass", melody_part="Soprano")
+	tm_alto_bass = TranslationModel(phrase_path="data/Alto_Bass_translation_model_major_rhythm.txt", note_path="data/Alto_Bass_translation_model_major.txt", harmony_part="Bass", melody_part="Soprano")
+
+	print "TM S B: " + str(e.evaluate_translation_model(tm_soprano_bass))
+	print "TM A B: " + str(e.evaluate_translation_model(tm_alto_bass))'''
+	print "+++ACTUAL_BACH+++"
+	e = ScoreEvaluator(corpus.parse('bach/bwv390'))
+	e.evaluate()
+	print "+++BACH+++"
+	e1 = ScoreEvaluator(converter.parse("bwv390_with_weights.xml"))
+	e1.evaluate()
+	print "+++BACH_SKIP_GRAM+++"
+	e5 = ScoreEvaluator(converter.parse("bach_with_skips.xml"))
+	e5.evaluate()
+	print "+++BACH_NOTE_BASED+++"
+	e4 = ScoreEvaluator(converter.parse("bach_note_based.xml"))
+	e4.evaluate()
+	print "+++OLD_BACH+++"
+	e3 = ScoreEvaluator(converter.parse("bach_scaled.xml"))
+	e3.evaluate()
+	print "+++TTLS+++"
+	e2 = ScoreEvaluator(converter.parse("ttls_with_weights.xml"))
+	e2.evaluate()
+
 if __name__ == "__main__":
     main()
