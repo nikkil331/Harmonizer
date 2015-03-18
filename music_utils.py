fb569f5e99479f12d476e61e6c2294d30ad9c6fa
nikkil331 <limni@seas.upenn.edu>
2015-03-18 15:03:26 -0400

    more functionality so that chords can be parsed and can get note harmonies for them

--- music_utils.py
+++ music_utils.py
@@ -60,8 +60,8 @@ def get_note_rep(note):
     elif note.isChord:
         return ','.join([get_note_rep(n) for n in note])
     elif note.isNote:
-        if note.accidental.fullname == 'double-flat' or \
-        note.accidental.fullname == 'double-sharp':
+        if note.accidental and (note.accidental.fullName == 'double-flat' or \
+           note.accidental.fullName == 'double-sharp'):
             note.pitch.getEnharmonic(inPlace=True)
         return note.nameWithOctave + ":" + str(note.quarterLength)
     else:
@@ -95,7 +95,6 @@ def make_stream_from_notes(notes):
     return s
 
 def make_stream_from_strings(notes):
-    print notes
     s = stream.Stream()
     for n_rep in notes:
         pitch, duration = n_rep.split(":")
@@ -114,7 +113,7 @@ def trim_stream(s, begin_offset, end_offset):
     section = s.getElementsByOffset(begin_offset, offsetEnd=end_offset, \
                                     mustBeginInSpan=False, includeEndBoundary=False, \
                                     includeElementsThatEndAtStart=False, \
-                                    classList=[note.Note, note.Rest, stream.Measure])
+                                    classList=[note.Note, chord.Chord, note.Rest, stream.Measure])
     section = copy.deepcopy(section)
     if len(section) > 0:
         # trim beginning
@@ -194,8 +193,9 @@ def put_notes_in_measures(measure_stream, note_stream):
 
 def get_barbershop_data():
     scores = []
-    for filename in os.listdir("Harmonizer/data/barbershop/split"):
-        scores.append("Harmonizer/data/barbershop/split/{0}".format(filename))
+    for filename in os.listdir("data/barbershop_scores/split"):
+        scores.append("data/barbershop_scores/split/{0}".format(filename))
+
     return scores
 
 
