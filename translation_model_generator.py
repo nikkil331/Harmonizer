930540f12535a8413b4b6d742ff6358bf93bb9db
rigeljs <rigeljs@gmail.com>
2015-03-26 12:12:47 -0400

    jesus christ

--- translation_model_generator.py
+++ translation_model_generator.py
@@ -25,8 +25,8 @@ class TranslationModelGenerator(object):
         begin_offset = 0.0
 
         for melody_note in melody.flat.notesAndRests:
-            if melody_note.isNote and melody_note.pitch < limits[self._melody_part][0] or \
-                            melody_note.pitch > limits[self._melody_part][1]:
+            if melody_note.isNote and (melody_note.pitch < limits[self._melody_part][0] or
+                                               melody_note.pitch > limits[self._melody_part][1]):
                 continue
             if self._phrase_based_mode:
                 melody_phrase.append(get_note_rep(melody_note))
