4733107f9cb8f83715c9bdac14b291701a824701
nikkil331 <limni@seas.upenn.edu>
2015-05-10 16:48:45 -0400

    don't include suffixes since we're assuming they're fixed

--- compose/composition.py
+++ compose/composition.py
@@ -42,12 +42,12 @@ class Composition(object):
         new_part = d.hyp_to_stream(hyp)
         return (name, new_part, decoder.get_score(hyp))
 
-    def best_new_part(self, parts, directory, lm_suffix, note_suffix, phrase_suffix):
-        second_parts = map(lambda x: self.create_part(x, '{0}/{1}_{2}'.format(directory, x, lm_suffix),
-                                            ['{0}/{1}_{2}_{3}'.format(directory, y[0],x, phrase_suffix) 
-                                                for y in self._parts],
-                                            ['{0}/{1}_{2}_{3}'.format(directory, y[0],x, note_suffix) 
-                                                for y in self._parts]), 
+    def best_new_part(self, parts, directory):
+        second_parts = map(lambda x: self.create_part(x, '{0}/{1}_language_model.txt'.format(directory, x,),
+                                            ['{0}/{1}_{2}_translation_model_rhythm.txt'\
+                                                .format(directory, y[0], x) for y in self._parts],
+                                            ['{0}/{1}_{2}_translation_model.txt'\
+                                                .format(directory, y[0], x) for y in self._parts]), 
                                         parts)
         winner = max(second_parts, key=lambda x: x[2])
         self._parts.append((winner[0], winner[1]))
@@ -67,17 +67,12 @@ def main():
     argparser.add_argument("--melody_name", dest="melody_name", default="Soprano",
                     help="Name of melody part")
     argparser.add_argument("--part_names", dest="part_names", default="Alto,Tenor,Bass", 
-                    help="Names of parts to generate")
+                    help="Names of parts to generate.")
     argparser.add_argument("--output_file", dest="output", default="output.xml",
                     help="Path to write the composition to")
     argparser.add_argument("--directory", dest="dir", default=".",
                     help="Directory where models live. Default is the current directory.")
-    argparser.add_argument("--tm_note_suffix", dest="tm_note_suffix", default="translation_model_major.txt",
-                     help="Filename suffix for note translation models")
-    argparser.add_argument("--tm_phrase_suffix", dest="tm_phrase_suffix", default="translation_model_major_rhythm.txt",
-                     help="Filename suffix for phrase translation models")
-    argparser.add_argument("--lm_suffix", dest="lm_suffix", default="language_model_major.txt",
-                     help="Filename suffix for language models")
+
     args = argparser.parse_args()
 
     test_song = corpus.parse(args.melody)
@@ -86,8 +81,7 @@ def main():
     c = Composition()
     c.add_part(args.melody_name, test_song.parts[args.melody_name])
     while parts:
-        parts.remove(c.best_new_part(parts, args.dir, 
-            args.lm_suffix, args.tm_note_suffix, args.tm_phrase_suffix))
+        parts.remove(c.best_new_part(parts, args.dir))
     c.save(args.output)
 
 if __name__ == "__main__":
